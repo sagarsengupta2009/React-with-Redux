e176af58b9d82751751ba292c8a329cf26bf5b72
Sagar Sengupta <sagarsengupta2009@gmail.com>
2025-06-01 17:52:14 +0530

    first commit

---
+++ src/components/Counter.js
@@ -0,0 +1,40 @@
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
+  console.log(showCounter);
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: "increase", payload: 5 });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {showCounter && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
---
+++ src/store/index.js
@@ -0,0 +1,32 @@
+import { createStore } from "redux";
+
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
+  if (action.type === "increment") {
+    return {
+      ...state,
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.payload,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  if (action.type === "toggle") {
+    return {
+      ...state,
+      showCounter: !state.showCounter,
+    };
+  }
+  return state;
+};
+const store = createStore(counterReducer);
+
+export default store;
